effe8591ea1fc8bc48267984219e98765ee50df3
Drew Douglass <drew@creatingdrew.com>
2015-09-24 16:56:56 -0500

    Added new modules.
    Added postcss-center and post-css-cssstats for fun stats.

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,17 @@ var precss = require('precss');
 var rucksack = require('rucksack-css');
 /* https://github.com/cssdream/cssgrace */
 var cssgrace = require('cssgrace');
+/* https://github.com/jedmao/postcss-center */
+var postcsscenter = require('postcss-center');
+/* https://github.com/cssstats/postcss-cssstats */
+var cssstats = require('postcss-cssstats');
+var postcss = require('gulp-postcss');
 
 gulp.task('default', function(){
 	var processors = [ 
 		precss(),  
 		rucksack(),
+		postcsscenter(),
 		autoprefixer( { browsers: ['> 1%'] } ),
 		//Run this last to fix any older browser issues possible 
 		require('cssgrace')
@@ -21,4 +27,18 @@ gulp.task('default', function(){
 	return gulp.src('./src/*.css')
 	.pipe(postcss(processors))
 	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('cssstats', function() {
+    return gulp
+        .src('./dist/style.css')
+        .pipe(
+            postcss([
+                cssstats(
+                    function(stats) {
+                        console.log(stats);
+                    }
+                )
+            ])
+        );
 });
